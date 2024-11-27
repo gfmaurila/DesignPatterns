645137d8b6428659e659c4bc840914c38116ddb7
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-11-26 22:34:08 -0300

    KISS

---
+++ KISS/KissService.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.KISS;
+
+public class KissService
+{
+
+    //Exemplo 2 - Com KISS(Certo)
+
+    public string GetStatus(int statusCode)
+    {
+        return statusCode switch
+        {
+            1 => "Success",
+            2 => "Error",
+            3 => "Pending",
+            _ => "Unknown"
+        };
+    }
+}
--- YAGNI/OrderService.cs
+++ YAGNI/OrderService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.YAGNI;
+﻿namespace DesignPatterns.YAGNI;
 
 
 //YAGNI - You Aren't Gonna Need It (Você Não Vai Precisar Disso)
